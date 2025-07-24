8b23d1c16b83bdfbf2ca8696bb45d908512ab55e
thelastpsycho <krisnatha@live.com>
2025-07-24 17:37:26 +0800

    Update AuthMiddleware.php

--- app/Http/Middleware/AuthMiddleware.php
+++ app/Http/Middleware/AuthMiddleware.php
@@ -8,29 +8,35 @@ use Exception;
 
 class AuthMiddleware
 {
-    private $secret = 'mas-fiq';
+    // private $secret = 'mas-fiq';    
     public function handle($request, Closure $next)
     {
-        $token = null;
-
+        // $token = null;
         // Ambil token dari header Authorization
         $authHeader = $request->header('Authorization');
-        if ($authHeader && preg_match('/Bearer\s(\S+)/', $authHeader, $matches)) {
-            $token = $matches[1];
+        if (!$authHeader && !preg_match('/Bearer\s(\S+)/', $authHeader, $matches)) {
+            \Log::error('token noe found');
+            // $token = $matches[1];
+            return response()->json(['error' => 'token gak ada'], 401)
         }
 
-        if (!$token) {
-            return response()->json(['error' => 'Token not found please login'], 401);
-        }
+        // if (!$token) {
+        //     return response()->json(['error' => 'Token not found please login'], 401);
+        // }
+        $token = $matches[1];
+        $secret = env('JWT_SECRET', 'fallback-secret'); // ingat update .env
 
         try {
-            $decoded = JWT::decode($token, $this->secret, ['HS256']);
+            // $decoded = JWT::decode($token, $this->secret, ['HS256']);
+            $decoded = JWT::decode($token, new Key($secret, 'HS256'));
             // Tambahkan user ke request jika perlu
             $request->auth = $decoded;
+            return $next($request);
         } catch (Exception $e) {
+            \Log::error('Auth Middleware: Token validation failed - ' . $e->getMessage());
             return response()->json(['error' => 'Invalid or expired token'], 401);
         }
 
-        return $next($request);
+        // return $next($request);
     }
 }
