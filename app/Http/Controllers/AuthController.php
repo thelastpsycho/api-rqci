b55ac8c41bcb7e21d478c7c58cd86b9b9febf386
thelastpsycho <krisnatha@live.com>
2025-07-24 17:27:49 +0800

    Update AuthController.php
    add has and key

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,23 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 
 class AuthController extends Controller
 {
-    private $secret = 'mas-fiq';
+    // private $secret = 'mas-fiq';
 
     public function login(Request $request)
     {
         $username = $request->input('username');
         $password = $request->input('password');
 
-        $user = DB::table('users')->where('username', $username)->where('password', $password)->first();
+        if (!$username || !$Password) {
+            return response()->json->(['error' => 'username password required bro'], 400)
+        }
 
-        if (!$user) {
+        //fetch dari database
+        $user = DB::table('users')->where('username', $username)->first();
+        //$user = DB::table('users')->where('username', $username)->where('password', $password)->first();
+
+        //verifikasi user
+        if (!$user || !Hash::check($password, $user->password)) {
             return response()->json(['error' => 'Invalid credentials'], 401);
         }
+        // if (!$user) {
+        //     return response()->json(['error' => 'Invalid credentials'], 401);
+        // }
 
         $payload = [
             'sub' => $user->id,
@@ -31,9 +43,10 @@ class AuthController extends Controller
 
         ];
 
+        $secret = env('JWT_SECRET', 'fallback-secret'); // taruh di .env itu yang mas-fiq
         $token = JWT::encode($payload, $this->secret, 'HS256');
 
-        return response()->json(['token' => $token]);
+        return response()->json(['token' => $token], 200);
     }
 
     public function me(Request $request)
@@ -45,15 +58,18 @@ class AuthController extends Controller
         }
 
         $token = $matches[1];
+        $secret = env('JWT_SECRET', 'fallback-secret')
 
         try {
-            $decoded = JWT::decode($token, $this->secret, ['HS256']);
+            $decoded = JWT::decode($token, new Key($secret, 'HS256'));
+            // $decoded = JWT::decode($token, $this->secret, ['HS256']);
             return response()->json([
                 'id' => $decoded->sub,
                 'username' => $decoded->username,
                 'iss' => $decoded->iss ,
-            ]);
+            ], 200);
         } catch (\Exception $e) {
+            \Log::error('Toket validation gagal bro: ' . $e->getMessage());
             return response()->json(['error' => 'Invalid or expired token'], 401);
         }
     }
